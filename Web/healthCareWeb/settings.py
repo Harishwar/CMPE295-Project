75e160612ea7e491e7dfb2c694afc96d619b5f0c
nsanthoshkumar <sanatom.sjsu@gmail.com>
2015-02-23 06:01:04 -0800

    removing email settings

--- Web/healthCareWeb/settings.py
+++ Web/healthCareWeb/settings.py
@@ -88,8 +88,8 @@ USE_TZ = True
 #http://stackoverflow.com/questions/11916754/djangosetting-up-email
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'dataminers239@gmail.com'
-EMAIL_HOST_PASSWORD = 'dataminer'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 # Static files (CSS, JavaScript, Images)
