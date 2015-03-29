ec8ee48b93291d1590ba5355ec18308b4d9a855a
nsanthoshkumar <sanatom.sjsu@gmail.com>
2015-03-28 17:11:35 -0700

    adding delete user, login and authenticate

--- Web/webapplication/doctorsView/views.py
+++ Web/webapplication/doctorsView/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.core.context_processors import request
 from urllib2 import HTTPError
-from models import User
+from models import Users
 import logging
 import datetime
 from django.http.response import JsonResponse, HttpResponseRedirect,HttpResponse
@@ -11,6 +11,9 @@ from django.core.urlresolvers import reverse
 import doctorsView
 from doctorsView.models import SensorUser, Allergies,UserAllergies,UserVaccination
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
@@ -20,24 +23,26 @@ logger = logging.getLogger()
 def index(request):
     return render(request,"registerUser.html");
 
+
 def search(request):
     return render(request,"list.html");
 
 def registerUser(request):
     try:
         logger.debug("Obtaining db fields")
-        registration=User()
+        registration=Users()
         registration.first_name=request.POST.get('FirstName')
         registration.last_name=request.POST.get('LastName')
         registration.email=request.POST.get('email')
         registration.user_id=request.POST.get('email')
         registration.dob=request.POST.get('dob')
         registration.address=request.POST.get('address')
+        registration.password="test"
         #need to convert to a timezone as it throws an exception
         registration.date_created=datetime.datetime.now()
         registration.date_modified=datetime.datetime.now()
         registration.save()
-        saved_details=User.objects.get(id=registration.id)
+        saved_details=Users.objects.get(id=registration.id)
         send_mail('HealthCareWeb registration', 'Hi,\n You have successfully registered for Enhanced Health Care Web.Please find below the username:'+saved_details.email+' \n password:', 'saninnovator@gmail.com',[saved_details.email], fail_silently=True)
         #saved_details=User.objects.filter(id=created_id).values()
         #serialized_obj = serializers.serialize('json', [ saved_details, ])
@@ -61,7 +66,7 @@ def addSensor(request):
         sensor_details=SensorUser()
         email=request.POST.get('email')
         print email
-        user_object=User.objects.get(email=email)
+        user_object=Users.objects.get(email=email)
         sensor_details.user_id=user_object
         sensor_details.sensor_id=request.POST.get('SensorID')
         sensor_details.date_created=datetime.datetime.now()
@@ -70,15 +75,16 @@ def addSensor(request):
 
 #Method for handling the profile view
 #https://docs.djangoproject.com/en/1.7/topics/serialization/
+@login_required(login_url='/doctorsView/login/')
 def viewUsers(request):
-    context={'users':User.objects.filter()}
+    context={'users':Users.objects.filter()}
     return render(request,'viewUsers.html',context)
 
 #Fetches users by lastname
 @csrf_exempt
 def getUserByLastName(request):
     last_name=request.GET.get('searchTerm')
-    user_result=User.objects.filter(last_name=last_name)
+    user_result=Users.objects.filter(last_name=last_name)
     context={'search_term':user_result}
     return render(request,'viewUser.html',context)
 
@@ -89,7 +95,7 @@ def getAllergiesList(request):
     return render(request,'addAllergies.html',context)
 
 def addUserAllergies(request):
-    user_object=User.objects.get(email=request.POST.get('email'))
+    user_object=Users.objects.get(email=request.POST.get('email'))
     print request.POST.getlist('allergy')
     for allergy_name in request.POST.getlist('allergy'):
         user_allergies=UserAllergies();
@@ -105,7 +111,7 @@ def addUserAllergies(request):
 
 
 def addUserVaccination(request):
-    user_object=User.objects.get(email=request.POST.get('email'))
+    user_object=Users.objects.get(email=request.POST.get('email'))
     user_vaccination=UserVaccination()
     user_vaccination.vaccination_desc=request.POST.get('vaccination')
     user_vaccination.user_id=user_object
@@ -118,20 +124,59 @@ def addUserVaccination(request):
 
 def deleteUser(request):
     user_id=request.POST.get('email')
-    User.objects.filter(email=user_id).delete();
+    Users.objects.filter(email=user_id).delete();
     print user_id
     return JsonResponse({'status':204,"result":"User Deleted Successfully"})
 
-'''def deleteUserAllergy(request):
+def deleteUserAllergy(request):
     user_id=request.POST.get('email')
     allergy_name=request.POST.get('allergy_name')
-    usr_id=User.objects.get(email=user_id).id
+    usr_id=Users.objects.get(email=user_id).id
     allergy_id=Allergies.objects.get(allergy_name=allergy_name).id
-    UserAllergies.objects.get(id=usr_id,allergy_id=allergy_id).delete();
-    return JsonResponse({'status':204,"result":"User Allergy Deleted Successfully"}) '''
+    print usr_id
+    print allergy_id
+    UserAllergies.objects.get(user_id=usr_id,allergy_id=allergy_id).delete();
+    return JsonResponse({'status':204,"result":"User Allergy Deleted Successfully"})
+
+
+def login_user(request):
+    if(request.session.get('user_id')):
+        print request.session.get('user_id')
+        context={'users':Users.objects.filter()}
+        return render(request,'viewUsers.html',context)
+    else:
+        if request.method=='GET':
+            return render(request,"list.html");
+        
+        else:
+            username=request.POST['email']
+            print username
+            password=request.POST['password']
+            print password
+            user=authenticate(username=username,password=password)
+            print user
+            if user is not None:
+                request.session['user_id']=user.username;
+                print request.session.get('user_id')
+                context={'users':Users.objects.filter()}
+                return render(request,'viewUsers.html',context)
+            else:
+                return HttpResponse("Invalid user");
+
+
+def authenticate(username,password):
+    try:
+        return Users.objects.get(email=username,password=password)
+    except Users.DoesNotExist:
+        return None 
+    #print Users.objects.get(email=username,password=password)
+    
+    
+#def updateUserProfile(request):
+    
 #edit users
 #patient visit history
 #login module
 #password encryption
-#deleteUser
+
 
