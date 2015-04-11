7857aa228c46cf530c8dc6cedda992c162eda6d3
nsanthoshkumar <sanatom.sjsu@gmail.com>
2015-04-11 16:17:33 -0700

    graphs for all users and specific user

--- Web/webapplication/doctorsView/views.py
+++ Web/webapplication/doctorsView/views.py
@@ -30,41 +30,45 @@ def search(request):
     return render(request,"list.html");
 
 def dashboard(request):
-    return render(request,"dashboard.html");
+    if request.method=="GET" and request.session.get('user_id'):
+        return render(request,"dashboard.html")
+    else:
+        return render(request,'index.html')
 # def addPatient(request):
 #     return render(request,"registerUser.html");
 
 def addPatient(request):
-    if request.method=="GET":
-        return render(request,"registerUser.html");
-    else:
-    
-        try:
+    try:
+        if request.method=="GET" and request.session.get('user_id'):
+            return render(request,"registerUser.html");
+        elif request.method=="POST" and request.session.get('user_id'):
             logger.debug("Obtaining db fields")
-            registration=Users()
-            registration.first_name=request.POST.get('FirstName')
-            registration.last_name=request.POST.get('LastName')
-            registration.email=request.POST.get('email')
-            registration.user_id=request.POST.get('email')
-            registration.dob=request.POST.get('dob')
-            registration.address=request.POST.get('address')
-            registration.password="test"
-            #need to convert to a timezone as it throws an exception
-            registration.date_created=datetime.datetime.now()
-            registration.date_modified=datetime.datetime.now()
+            registration = Users()
+            registration.first_name = request.POST.get('FirstName')
+            registration.last_name = request.POST.get('LastName')
+            registration.email = request.POST.get('email')
+            registration.user_id = request.POST.get('email')
+            registration.dob = request.POST.get('dob')
+            registration.address = request.POST.get('address')
+            registration.password = "test"
+            # need to convert to a timezone as it throws an exception
+            registration.date_created = datetime.datetime.now()
+            registration.date_modified = datetime.datetime.now()
             registration.save()
-            saved_details=Users.objects.get(id=registration.id)
-            send_mail('HealthCareWeb registration', 'Hi,\n You have successfully registered for Enhanced Health Care Web.Please find below the username:'+saved_details.email+' \n password:', 'saninnovator@gmail.com',[saved_details.email], fail_silently=True)
-            #saved_details=User.objects.filter(id=created_id).values()
-            #serialized_obj = serializers.serialize('json', [ saved_details, ])
-        except HTTPError:
-            logger.debug("Error Handling Registration")
-            return "Error"
+            saved_details = Users.objects.get(id=registration.id)
+            send_mail('HealthCareWeb registration', 'Hi,\n You have successfully registered for Enhanced Health Care Web.Please find below the username:' + saved_details.email + ' \n password:', 'saninnovator@gmail.com', [saved_details.email], fail_silently=True)
+                #saved_details=User.objects.filter(id=created_id).values()
+                #serialized_obj = serializers.serialize('json', [ saved_details, ])
+            #return JsonResponse({"email":saved_details.email,"id":saved_details.id})
+            return HttpResponseRedirect(reverse('addSensor')+'?email='+saved_details.email)
+        else:
+            return render(request, 'index.html')
+            
+    except HTTPError:
+                logger.debug("Error Handling Registration")
+                return "Error"        
 
-
-    #return JsonResponse({"email":saved_details.email,"id":saved_details.id})
-    return HttpResponseRedirect(reverse('addSensor')+'?email='+saved_details.email)
-
+    
 
 #redirect(doctorsView.views.addSensor, saved_details.email)
 
@@ -72,11 +76,11 @@ def addPatient(request):
 #Method to handle the user sensor data
 #@login_required(login_url='/doctorsView/login/')
 def addSensor(request): 
-    if request.session.get('user_id'):
-        if request.method=='GET':
+    try:
+        if request.method =='GET' and request.session.get('user_id'):
             return render(request,"addSensor.html");
-        else:
-            sensor_details=SensorUser()
+        elif request.method=='POST' and request.session.get('user_id'):
+            sensor_details = SensorUser()
             email=request.POST.get('email')
             print email
             user_object=Users.objects.get(email=email)
@@ -86,72 +90,105 @@ def addSensor(request):
             sensor_details.save()
             #return JsonResponse({"status":201,"result":"Sensor Relation Added"})
             return getAllergiesList(request)    
-    else:
-        return render(request,"list.html");
-
+        else:
+            return render(request,"index.html");
+    except:
+            return HttpResponse("Service Error!!!")
+    
 #Method for handling the profile view
 #https://docs.djangoproject.com/en/1.7/topics/serialization/
-@login_required(login_url='/doctorsView/login/')
+#@login_required(login_url='/doctorsView/login/')
 def viewUsers(request):
-    context={'users':Users.objects.filter()}
-    return render(request,'viewUsers.html',context)
-
+    try:
+        if request.method=='GET' and request.session.get('user_id'):
+            context={'users':Users.objects.filter()}
+            return render(request,'viewUsers.html',context)
+        else:
+            return render(request,'index.html')
+        
+    except:
+        return HttpResponse("Service Error!!!")
 #Fetches users by lastname
 @csrf_exempt
 def getUserByLastName(request):
-    last_name=request.GET.get('searchTerm')
-    user_result=Users.objects.filter(last_name=last_name)
-    context={'users':user_result}
-    return render(request,'viewUsers.html',context)
-
+    try:
+        if request.method=='GET' and request.session.get('user_id'):
+            last_name=request.GET.get('searchTerm')
+            user_result=Users.objects.filter(last_name=last_name)
+            context={'users':user_result}
+            return render(request,'viewUsers.html',context)
+        else:
+            return render(request,'index.html')
+    except:
+        return HttpResponse("Service Error!!!")
 #returns the list of allergies
 def getAllergiesList(request):
-    #print serializers.serialize("json", Allergies.objects.all(),fields=('allergy_name'))
-    context={'allergies':Allergies.objects.values('allergy_name')}
-    return render(request,'addAllergies.html',context)
+    try:
+        if request.method=='GET' and request.session.get('user_id'):
+            #print serializers.serialize("json", Allergies.objects.all(),fields=('allergy_name'))
+            context={'allergies':Allergies.objects.values('allergy_name')}
+            return render(request,'addAllergies.html',context)
+        else:
+            return render(request,'index.html')
+    except:
+        return HttpResponse("Service Error!!!")
 
 @csrf_exempt
 def addUserAllergies(request):
-    print request.POST.get('email')
-    email=request.POST.get('email')
-    user_object=Users.objects.get(email=email)
-    print "db",user_object.email
-    print request.POST.getlist('allergy')
-    for allergy_name in request.POST.getlist('allergy'):
-        user_allergies=UserAllergies();
-        print Allergies.objects.get(allergy_name=allergy_name)
-        allergy_object=Allergies.objects.get(allergy_name=allergy_name)
-        user_allergies.user_id=user_object
-        user_allergies.allergy_id=allergy_object
-        user_allergies.date_created=datetime.datetime.now()
-        user_allergies.date_modified=datetime.datetime.now()
-        user_allergies.save()
-    #allergy_object=Allergies.objects.filter(allergy_name=list_allergies)
-    #for allergy in list_allergies:
-    return JsonResponse({"status":201,"result":"Allergies User Relation Added"})
-
+    try:
+        if request.method=='POST' and request.session.get('user_id'):
+            print request.POST.get('email')
+            email=request.POST.get('email')
+            user_object=Users.objects.get(email=email)
+            print "db",user_object.email
+            print request.POST.getlist('allergy')
+            for allergy_name in request.POST.getlist('allergy'):
+                user_allergies=UserAllergies();
+                print Allergies.objects.get(allergy_name=allergy_name)
+                allergy_object=Allergies.objects.get(allergy_name=allergy_name)
+                user_allergies.user_id=user_object
+                user_allergies.allergy_id=allergy_object
+                user_allergies.date_created=datetime.datetime.now()
+                user_allergies.date_modified=datetime.datetime.now()
+                user_allergies.save()
+                #allergy_object=Allergies.objects.filter(allergy_name=list_allergies)
+                #for allergy in list_allergies:
+                return JsonResponse({"status":201,"result":"Allergies User Relation Added"})
+            else:
+                return render(request,'index.html')
+    except:
+        return HttpResponse("Service Error!!!")
 
 def addUserVaccination(request):
-    if request.method=="GET":
-        return render(request,'vaccination.html')
-    else:
-        user_object=Users.objects.get(email=request.POST.get('email'))
-        print 'email from add sensor',request.POST.get('email')
-        user_vaccination=UserVaccination()
-        user_vaccination.vaccination_desc=request.POST.get('vaccination')
-        user_vaccination.user_id=user_object
-        user_vaccination.date_visited=request.POST.get('date_visited')
-        user_vaccination.date_created=datetime.datetime.now()
-        user_vaccination.date_modified=datetime.datetime.now()
-        user_vaccination.save()
-        return JsonResponse({"status":201,"result":"User Vaccination Added"})
+    try:
+        if request.method=="GET" and request.session.get('user_id'):
+            return render(request,'vaccination.html')
+        elif(request.method=='POST' and request.session.get('user_id')):
+            user_object=Users.objects.get(email=request.POST.get('email'))
+            print 'email from add sensor',request.POST.get('email')
+            user_vaccination=UserVaccination()
+            user_vaccination.vaccination_desc=request.POST.get('vaccination')
+            user_vaccination.user_id=user_object
+            user_vaccination.date_visited=request.POST.get('date_visited')
+            user_vaccination.date_created=datetime.datetime.now()
+            user_vaccination.date_modified=datetime.datetime.now()
+            user_vaccination.save()
+            return JsonResponse({"status":201,"result":"User Vaccination Added"})
+        else:
+            return render(request,'index.html')
+    except:
+        return HttpResponse("Service Error!!!")
 #def updateUser(request):
 
 def deleteUser(request):
-    user_id=request.POST.get('email')
-    Users.objects.filter(email=user_id).delete();
-    print user_id
-    return JsonResponse({'status':204,"result":"User Deleted Successfully"})
+    try:
+        user_id=request.POST.get('email')
+        Users.objects.filter(email=user_id).delete();
+        print user_id
+        return JsonResponse({'status':204,"result":"User Deleted Successfully"})
+    except:
+        return HttpResponse("Service Error!!!")
+        
 
 def deleteUserAllergy(request):
     user_id=request.POST.get('email')
@@ -205,27 +242,62 @@ def logout_user(request):
     return HttpResponse("You're logged out.")
 
 def dashboard_req(request):
-    
-    email=request.GET.get('email')
-    print '-------->'
-    print load_user_data(email)
-    return JsonResponse(load_user_data(email),safe=False)
+    if request.method=='GET' and request.session.get('user_id'):
+        email=request.GET.get('email')
+        print '-------->'
+        print load_user_data(email)
+        return JsonResponse(load_user_data(email),safe=False)
 
+
+def dashboard_doc_req(request):
+    if request.method=='GET' and request.session.get('user_id'):
+        print '-------->'
+        print"Hiiii"
+        print load_all_users_data()
+        return JsonResponse(load_all_users_data(),safe=False)
+    
 def load_user_data(email):
-    cursor=connection.cursor()
-    cursor.execute("select * from SensorResults.crunched_results where user_id=%s",[email])
-    rows=cursor.fetchall()
-    rowsList=[]
-    for row in rows:
-        graph_obj=collections.OrderedDict()
-        if isinstance(row[1], datetime.datetime):
-            dateVal= row[1].isoformat()
-        graph_obj['x']=dateVal
-        graph_obj['y']=row[2]
-        graph_obj['user_id']=row[0]
-        rowsList.append(graph_obj)
-        print row
-    return json.dumps(rowsList)
+    try:
+        cursor=connection.cursor()
+        cursor.execute("select * from SensorResults.crunched_results where user_id=%s",[email])
+        rows=cursor.fetchall()
+        rowsList=[]
+        for row in rows:
+            graph_obj=collections.OrderedDict()
+            if isinstance(row[1], datetime.datetime):
+                dateVal= row[1].isoformat()
+            graph_obj['x']=dateVal
+            graph_obj['y']=row[2]
+            graph_obj['user_id']=row[0]
+            rowsList.append(graph_obj)
+            print row
+        connection.close()    
+        return json.dumps(rowsList)
+    except:
+        return "Could not process"
+    
+def load_all_users_data():
+    try:
+        print "entered load users"
+        cursor=connection.cursor()
+        cursor.execute("select * from SensorResults.crunched_results")
+        rows=cursor.fetchall()
+        print rows
+        rowsList=[]
+        for row in rows:
+            graph_obj=collections.OrderedDict()
+            if isinstance(row[1], datetime.datetime):
+                dateVal= row[1].isoformat()
+            graph_obj['x']=dateVal
+            graph_obj['y']=row[2]
+            graph_obj['user_id']=row[0]
+            rowsList.append(graph_obj)
+            print row
+        return json.dumps(rowsList)
+    except:
+        return "Could not process"
+
+
 #def updateUserProfile(request):
 
 #edit users
