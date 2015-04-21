d348f10d4be372fd63952fb6bb3e8af0672c773c
Harishwar <harish.tpl@gmail.com>
2015-04-21 01:32:18 -0700

    Cloud deploy fitwhiz.elasticbeanstalk.com

---
+++ Web/webapplication/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/harishwar/GitHub/CMPE295-Project/Web/webapplication/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
